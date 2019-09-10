be199b4ec7be99e5c1a5d5e41362a4c2cba49275
ISHAN PANDE <ishanpande@mail.usf.edu>
2019-09-10 19:53:05 -0400

    comit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+            string input = Console.ReadLine();
+
+            switch(int.Parse(input))
+            {
+                case 0: {
+                        Console.WriteLine("Need lots of practice");
+                        break;
+                        }
+                case 1:
+                    {
+                        Console.WriteLine("Looks like you have a little experience");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Wow! youve been doing this for a little while");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Please provide a valid input");
+                        break;
+                    }
+            }
+        }
+    }
+}
